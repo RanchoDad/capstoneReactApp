bc096b5962f96c1aafd454489421742d05d69082
RanchoDad <joshuagarber215@gmail.com>
2023-06-14 12:19:39 -0700

    homepage edit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -24,8 +24,7 @@ export default function HomePage() {
                 </div>
        
                 {/* <div className="col-sm-4"> */}
-                
-
+            
                 {/* </div> */}
             </div>
         </div>
